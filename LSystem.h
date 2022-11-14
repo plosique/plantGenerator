6be56cbb35b30df12c5a83fcdb5904654f2eeaea
Patrick Losique <plosique@gmail.com>
2022-11-13 20:10:10 -0700

    Koch curve test implementation

--- LSystem.h
+++ LSystem.h
@@ -2,6 +2,67 @@
 #include<vector>
 #include<map>
 #include<utility>
+#include<iostream>
+
+#include <SFML/Graphics.hpp>
+
+
+sf::Vector2f R90(sf::Vector2f xy){
+    return sf::Vector2f(-xy.y,xy.x);
+
+}
+
+sf::Vector2f R180(sf::Vector2f xy){
+    return sf::Vector2f(-xy.x,-xy.y);
+
+}
+
+sf::Vector2f R270(sf::Vector2f xy){
+    return sf::Vector2f(xy.y,-xy.x);
+
+}
+
+sf::Vector2f R(sf::Vector2f xy, float theta){
+   return sf::Vector2f(sin(theta) ) 
+}
+
+class turtle{
+    sf::Vector2f loc; 
+    sf::Vector2f dir; 
+
+    public:
+        turtle(sf::Vector2f _loc, sf::Vector2f _dir) :loc(_loc), dir(_dir) {};
+
+        sf::Vector2f getLoc(){
+            return loc;
+        }
+
+        sf::Vector2f getDir(){
+            return dir;
+        }
+
+        //update the turtle -- boolean indicates whether anything is actually drawn
+        bool processSymbol(char symbol){
+            bool res = false; 
+            switch(symbol){
+                case 'F':
+                    loc+=dir;
+                    res = true; 
+                    break;
+               case '-':
+                    dir = R270(dir);
+                    res = false;
+                    break;
+               case '+':
+                    dir = R90(dir);
+                    res = false;
+                    break;
+               default:
+                    std::cerr<<"Unrecognized Symbol"<<std::endl;
+            }
+            return res; 
+        }
+};
 
 
 //cf stands for context-free
@@ -36,3 +97,19 @@ std::string apply(std::string axiom,int depth,cfLSystem LSystem){
     return res; 
 }
 
+sf::VertexArray renderWord(std::string s,int startx, int starty){
+    int numSymbols = s.size();  
+    sf::VertexArray arr(sf::LinesStrip,numSymbols+1); 
+    sf::Vector2f loc = sf::Vector2f(float(startx), float(starty));  
+    sf::Vector2f dir(1,0);  
+    arr[0] = loc;
+    turtle todd(loc, dir);
+    for(int i=0;i<numSymbols;i++){
+        todd.processSymbol(s.at(i));
+        arr[i+1].position =  todd.getLoc(); 
+        arr[i+1].color = sf::Color::Green; 
+    }
+    
+    return arr; 
+
+}
--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
 #include "LSystem.h"
 #include<vector> 
 #include<iostream>
+#include <SFML/Graphics.hpp>
 
 
 int main(){
-    std::vector<std::pair<char,std::string>> productions(2);
-    productions[0] = std::make_pair('a',"ab");
-    productions[1] = std::make_pair('b',"a");
+    std::vector<std::pair<char,std::string>> productions(1);
+    productions[0] = std::make_pair('F',"F-F+F+FF-F-F+F");
 
     cfLSystem LSystem(productions);
-    for(int i=0;i<7;i++){
-        std::string output = apply("a", i, LSystem);
-        std::cout<<output<<std::endl;
+    std::string axiom = "F-F-F-F"; 
+    std::string word = apply(axiom, 3, LSystem);
+    sf::VertexArray vArr =  renderWord(word,100,100); 
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        //window.draw(shape);
+        window.draw(vArr);
+        window.display();
     }
+
     return 0;
 }
