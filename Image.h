1987be10f977176a561603af4542c987257c089a
Patrick Losique <Patrick Losiuqe>
2022-11-17 10:27:43 -0700

    handled compiler warnings

--- Image.h
+++ Image.h
@@ -3,13 +3,13 @@
 
 
 void scale(sf::VertexArray &va,float scalingFactor){
-    for(int i=0;i<va.getVertexCount();i++){
+    for(unsigned int i=0;i<va.getVertexCount();i++){
         va[i].position*=scalingFactor; 
     }
 }
 
 void translate(sf::VertexArray &va,sf::Vector2f translation){
-    for(int i=0;i<va.getVertexCount();i++){
+    for(unsigned int i=0;i<va.getVertexCount();i++){
         va[i].position+=translation; 
     }
 }
@@ -22,7 +22,7 @@ void scaleAndCenter(sf::VertexArray &va, int dimx,int dimy){
     maxx = minx;
     miny = va[0].position.y;
     maxy = miny;
-    for(int i=0;i<va.getVertexCount();i++){
+    for(unsigned int i=0;i<va.getVertexCount();i++){
         sf::Vertex v = va[i]; 
         maxy = std::max(maxy,v.position.y);
         maxx = std::max(maxx,v.position.x);
@@ -32,10 +32,11 @@ void scaleAndCenter(sf::VertexArray &va, int dimx,int dimy){
     sf::Vector2f translation0(-minx,-miny);
     translate(va,translation0);
 
+
     float scalingFactor = ((4*fdimx)/5)/(maxx-minx);
     scale(va,scalingFactor);
 
-    sf::Vector2f translation1(dimx/10,dimy/10);
+    sf::Vector2f translation1(fdimx/10,fdimy/10);
     translate(va,translation1);
 
     
